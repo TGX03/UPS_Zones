396512263b79bc0b86009691fbd317aca17ef64f
TGX03 <weber-lars@web.de>
2021-01-25 20:35:18 +0100

    Make Country an internal class

--- src/main/java/de/tgx03/ups/Reformatter.java
+++ src/main/java/de/tgx03/ups/Reformatter.java
@@ -110,4 +110,40 @@ public class Reformatter {
         output.write(outputStream);
     }
 
+    /**
+     * A class representing a country and its corresponding zone IDs
+     */
+    private static class Country {
+
+        public final String name;
+        public final short expressID;
+        public final short standardID;
+        public final boolean standard;
+        public final boolean expedited;
+
+        /**
+         * @param name The name of this country
+         * @param express Its ID for express shipping
+         * @param standard Its ID for standard or expedited shipping
+         * @param standardAvailable Whether standard shipping is available
+         * @param expedited Whether standard is actually expedited
+         */
+        public Country (String name, short express, short standard, boolean standardAvailable, boolean expedited) {
+            this.name = name;
+            this.expressID = express;
+            this.standardID = standard;
+            this.standard = standardAvailable;
+            this.expedited = expedited;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+    }
 }
